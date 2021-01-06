0cdb63602f2db76c71fe1874abc4ef3e22153f95
ClericMT <43632579+ClericMT@users.noreply.github.com>
2021-01-06 10:54:58 -0800

    Update code.py

--- code.py
+++ code.py
@@ -1,7 +1,8 @@
 import math
+import time
 
 stock_list = []
-receipt_num = 0000
+receipt_num = 0
 
 class Stock:
     def __init__(self, name, code, bundle_sizes):
@@ -12,7 +13,7 @@ class Stock:
         self.total_order = {}
         self.total_price = 0
 
-    def num_of_bundles(self):
+    def num_of_bundles(self): #COMPUTE BUNDLE SIZES
         bundles = (sorted(self.bundle_sizes.keys(), reverse = True)) #Sorts available bundle sizes from highest (least bundle_sizes required) to lowest
         items_left = self.num_of_stock #Remainder of items to bundle
         for bundle in range(len(bundles)): #Compute how much stock can be bundled in the available sizes (from highest available size -> lowest available size)
@@ -23,7 +24,7 @@ class Stock:
             self.total_price += self.bundle_sizes[bundles[bundle]] * num_of_bundle #Updates total price
         self.print_total_order()
 
-    def print_total_order(self): #Format strings for legibility and log
+    def print_total_order(self): #FORMAT ORDER
         if self.num_of_stock != 0:
             print(' ' + str(self.num_of_stock) + ' ' + str(self.code) + ' $' + str("{:.2f}".format(self.total_price)))
             for bund_size, bund_info in self.total_order.items():
@@ -34,24 +35,27 @@ class Stock:
 vegemite_scroll = Stock('Vegemite Scroll', 'VS5', {3: 6.99, 5: 8.99}) #Creates new stock items
 blueberry_muffin = Stock('Blueberry Muffin', 'MB11', {2: 9.95, 5: 16.95, 8: 24.95})
 croissant = Stock('Croissant', 'CF', {3: 5.95, 5: 9.95, 9: 16.99})
-stock_list.extend([vegemite_scroll, blueberry_muffin, croissant]) #Appends to stock list ###Could also create an appendix?
+stock_list.extend([vegemite_scroll, blueberry_muffin, croissant]) #Appends to stock list
 
 while True: #Menu
     user_input = input('\n1. Add to order\n2. Finalise order\n3. Appendix\n')
-    if user_input == '1':
+    if user_input == '1': #ENTER CODES
         num_of_stock, item_code = input("Enter stock code: ").split()
         for item in stock_list:
             if item_code == item.code:
                 item.num_of_stock += int(num_of_stock)
-    elif user_input == '2':
-        print('\nOR#' + str("{0:0=8d}".format(receipt_num)))
+    elif user_input == '2': #COMPUTE ORDER
+        start = time.time()
+        print('OR#' + str("{0:0=8d}".format(receipt_num)))
         for stock in stock_list:
             stock.num_of_bundles()
             stock.total_order.clear() #Reset stock list for next order
             stock.total_price = 0
             stock.num_of_stock = 0
         receipt_num += 1
-    elif user_input == '3':
+        end = time.time()
+        print('Process time: ' + str(end - start))
+    elif user_input == '3': #LIST STOCK CODES
         for stock in stock_list:
             print(stock.name + ': ' + stock.code)
 
